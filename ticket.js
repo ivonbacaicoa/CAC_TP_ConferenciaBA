ed45faf1d7e92e30bbb679093db8114dbb00f627
Ivon Bacaicoa <contacto@ivonbacaicoa.com>
2023-05-25 20:05:18 -0300

    Solución números decimales
    Ya no se pueden usar números decimales en la operación

--- ticket.js
+++ ticket.js
@@ -1,32 +1,14 @@
-
-
-
-
 const button = document.querySelector("#btncalcular");
 
-const total = document.querySelector("#total"); 
-const cantegoria = document.querySelector("#categoria"); 
-const cantidad = document.querySelector("#cantidad"); 
+const total = document.querySelector("#total");
+const cantegoria = document.querySelector("#categoria");
+const cantidad = document.querySelector("#cantidad");
 
-
-function validarCampoTexto() {
-    var campoTexto = document.getElementById('miCampoTexto').value;
-    
-    if (campoTexto.trim() === '') {
-      alert('El campo de texto está vacío');
-      return false; // La validación falla
-    }
-    
-    // La validación pasa
-    return true;
+button.onclick = () => {
+  const cantidadValue = parseInt(cantidad.value);
+  if (cantidadValue > 0) {
+    total.value = "Total a pagar: $" + cantidadValue * 200 * categoria.value;
+  } else {
+    alert("Ingrese un número entero positivo");
   }
-
-
-button.onclick = () => { 
-    if (cantidad.value > 0) {
-    total.value = "Total a pagar: $"+ cantidad.value*200*categoria.value
-} else
-// console.log("No se puede realizar un producto con una variable negativa")
-alert('No es posible comprar una cantidad negativa')
-}
-
+};
