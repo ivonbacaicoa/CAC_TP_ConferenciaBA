3776e3a446c78ebd16390a5229297903a9285c2c
Ivon Bacaicoa <contacto@ivonbacaicoa.com>
2023-05-27 02:06:54 -0300

    Se agregan scripts
    Se agregan validaciones en el formulario

--- ticket.js
+++ ticket.js
@@ -1,14 +1,15 @@
 const button = document.querySelector("#btncalcular");
-
 const total = document.querySelector("#total");
-const cantegoria = document.querySelector("#categoria");
+const categoria = document.querySelector("#categoria");
 const cantidad = document.querySelector("#cantidad");
 
-button.onclick = () => {
-  const cantidadValue = parseInt(cantidad.value);
-  if (cantidadValue > 0) {
-    total.value = "Total a pagar: $" + cantidadValue * 200 * categoria.value;
-  } else {
-    alert("Ingrese un número entero positivo");
+  button.onclick = () => {
+    const cantidadValue = parseInt(cantidad.value);
+    if (cantidadValue > 0) {
+      total.value = "Total a pagar: $" + cantidadValue * 200 * categoria.value;
+      button.clicked = true; // Indicar que se ha presionado el botón 'Resumen'
+    } else {
+      alert("Ingrese un número entero positivo")
+    }
   }
-};
+
